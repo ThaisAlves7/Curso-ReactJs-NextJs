dc8563eb128902a24170b7cb869d1b58aaf225c3
ThaisAlves7 <75734682+ThaisAlves7@users.noreply.github.com>
2022-12-11 15:30:26 -0300

    Reac.lazy [ Geralmente usado para refatoração ]
    Suspense e divisão de codigo [ Code-Splitting ]
    A função do React.lazy é permitir a você renderizar uma importação dinâmica como se fosse um componente comum

---
+++ src/templates/Home/index-hook-children-clone-element.jsx
@@ -0,0 +1,56 @@
+import { Children, cloneElement, createContext, useContext, useState } from 'react';
+
+const s = {
+  style: {
+    fontSize: '60px',
+  },
+};
+
+const TurnOnOffContext = createContext();
+
+const TurnOnOff = ({ children }) => {
+  const [isOn, setIsOn] = useState(false);
+
+  // Obter a alteração de estado de isOn [Realiza no setIsOn] passando isso para onTurn
+  const onTurn = () => setIsOn((s) => !s);
+
+  return <TurnOnOffContext.Provider value={{ isOn, onTurn }}>{children}</TurnOnOffContext.Provider>;
+};
+
+const TurnedOn = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? children : null;
+};
+const TurnedOff = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? null : children;
+};
+
+const TurnButton = ({ ...props }) => {
+  const { isOn, onTurn } = useContext(TurnOnOffContext);
+
+  return (
+    <button onClick={onTurn} {...props}>
+      Turn {isOn ? 'OFF' : 'ON'}
+    </button>
+  );
+};
+
+const P = ({ children }) => <p {...s}>{children}</p>;
+
+export const Home = () => {
+  return (
+    <TurnOnOff>
+      <div>
+        <p>Oi</p>
+        <TurnedOn>
+          <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
+        </TurnedOn>
+        <TurnedOff>
+          <P>Aqui vem as coisas do OFF.</P>
+        </TurnedOff>
+        <TurnButton {...s} />
+      </div>
+    </TurnOnOff>
+  );
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,56 +1,24 @@
-import { Children, cloneElement, createContext, useContext, useState } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
+// import { LazyComponent } from './lazy-component';
 
-const s = {
-  style: {
-    fontSize: '60px',
-  },
+const loadComponent = () => {
+  console.log('Componente carregando...');
+  return import('./lazy-component');
 };
 
-const TurnOnOffContext = createContext();
-
-const TurnOnOff = ({ children }) => {
-  const [isOn, setIsOn] = useState(false);
-
-  // Obter a alteração de estado de isOn [Realiza no setIsOn] passando isso para onTurn
-  const onTurn = () => setIsOn((s) => !s);
-
-  return <TurnOnOffContext.Provider value={{ isOn, onTurn }}>{children}</TurnOnOffContext.Provider>;
-};
-
-const TurnedOn = ({ children }) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? children : null;
-};
-const TurnedOff = ({ children }) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? null : children;
-};
-
-const TurnButton = ({ ...props }) => {
-  const { isOn, onTurn } = useContext(TurnOnOffContext);
-
-  return (
-    <button onClick={onTurn} {...props}>
-      Turn {isOn ? 'OFF' : 'ON'}
-    </button>
-  );
-};
-
-const P = ({ children }) => <p {...s}>{children}</p>;
+const LazyComponent = lazy(loadComponent);
 
 export const Home = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <TurnOnOff>
-      <div>
-        <p>Oi</p>
-        <TurnedOn>
-          <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
-        </TurnedOn>
-        <TurnedOff>
-          <P>Aqui vem as coisas do OFF.</P>
-        </TurnedOff>
-        <TurnButton {...s} />
-      </div>
-    </TurnOnOff>
+    <div>
+      <p>
+        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
+          Show {show ? 'LC on screen' : 'LC is off screen'}
+        </button>
+      </p>
+      <Suspense fallback={<p>Carregando...</p>}>{show && <LazyComponent />}</Suspense>
+    </div>
   );
 };
