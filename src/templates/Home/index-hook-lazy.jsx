7c3386ca32ac2fbff4acfbad103ae1d42d56a456
ThaisAlves7 <75734682+ThaisAlves7@users.noreply.github.com>
2022-12-11 15:33:04 -0300

    Retorno ao index original de posts

---
+++ src/templates/Home/index-hook-lazy.jsx
@@ -0,0 +1,24 @@
+import React, { lazy, Suspense, useState } from 'react';
+// import { LazyComponent } from './lazy-component';
+
+const loadComponent = () => {
+  console.log('Componente carregando...');
+  return import('./lazy-component');
+};
+
+const LazyComponent = lazy(loadComponent);
+
+export const Home = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <p>
+        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
+          Show {show ? 'LC on screen' : 'LC is off screen'}
+        </button>
+      </p>
+      <Suspense fallback={<p>Carregando...</p>}>{show && <LazyComponent />}</Suspense>
+    </div>
+  );
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,24 +1,69 @@
-import React, { lazy, Suspense, useState } from 'react';
-// import { LazyComponent } from './lazy-component';
+import React from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
-const loadComponent = () => {
-  console.log('Componente carregando...');
-  return import('./lazy-component');
-};
+import './styles.css';
 
-const LazyComponent = lazy(loadComponent);
+import { Posts } from '../../components/Posts';
+import { loadPosts } from '../../utils/load-posts';
+import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
 
 export const Home = () => {
-  const [show, setShow] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(2);
+  const [searchValue, setSearchValue] = useState('');
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = searchValue
+    ? allPosts.filter((post) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    : posts;
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+    const postsAndPhotos = await loadPosts();
+
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
+
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+
+    posts.push(...nextPosts);
+
+    setPosts(posts);
+    setPage(nextPage);
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
 
   return (
-    <div>
-      <p>
-        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
-          Show {show ? 'LC on screen' : 'LC is off screen'}
-        </button>
-      </p>
-      <Suspense fallback={<p>Carregando...</p>}>{show && <LazyComponent />}</Suspense>
-    </div>
+    <section className="container">
+      <div className="search-container">
+        {!!searchValue && <h1>Search value: {searchValue}</h1>}
+
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
+      </div>
+
+      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+
+      {filteredPosts.length === 0 && <p>Não existem posts =( </p>}
+
+      <div className="button-container">
+        {!searchValue && <Button text="Load More Posts" onClick={loadMorePosts} disabled={noMorePosts} />}
+      </div>
+    </section>
   );
 };
